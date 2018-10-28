edca3ff0310586e87790138667188b64751a743f
Matthew Talbot <mdtalbot@gmail.com>
2018-10-28 17:25:08 -0400

    string methods

---
+++ string_methods.php
@@ -0,0 +1,11 @@
+<?php
+echo strlen("Hello world!"); // gives string length
+
+echo str_word_count("Hello world!"); // gives string word count
+
+echo strrev("Hello world!"); // reverses string
+
+echo strpos("Hello world!", "world"); // finds position(index value?) of word
+
+echo str_replace("world", "Dolly", "Hello world!"); // find and replace within string
+?>
