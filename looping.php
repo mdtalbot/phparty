7d8f1efa41394d8577c95da9ccffcc367000795e
Matthew Talbot <mdtalbot@gmail.com>
2018-11-04 23:07:36 -0500

    looping!

---
+++ looping.php
@@ -0,0 +1,16 @@
+<?php
+/* While loop */
+$x = 6;
+
+do {
+    echo "The number is: $x <br>";
+    $x++;
+} while ($x <= 5);
+?>
+
+<?php
+/* For loop */
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+?>
